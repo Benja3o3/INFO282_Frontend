d26f4c0bdcba289d0f2a662c7549dc067f485b29
Benja303 <anugamer303@gmail.com>
2023-08-23 10:08:41 -0400

    tarjeta

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import Map from "./Components/Map";
 import "./App.css";
 import Graphic from "./Components/Graphic";
+import Card from "./Components/Card";
 
 function App() {
   return (
     <div className="parent">
       <div className="info">
         <Graphic></Graphic>
+        <Card></Card>
       </div>
       <div className="map">
         <Map></Map>
