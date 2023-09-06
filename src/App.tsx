631c552ab145676a45a6168f3b565511c29895db
Benja303 <anugamer303@gmail.com>
2023-09-06 10:11:07 -0300

    Frontend actualizado

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Map from "./Components/Map";
+import Graphic from "./Components/Graphic";
 import "./App.css";
 
 function App() {
   return (
     <div className="parent">
       <div className="info">
+        <Graphic></Graphic>
       </div>
       <div className="map">
         <Map></Map>
--- src/Components/Map.tsx
+++ src/Components/Map.tsx
@@ -1,60 +1,131 @@
-import { MapContainer, TileLayer, GeoJSON } from "react-leaflet";
+import React, { useEffect, useState } from "react";
+import L from "leaflet";
 import "leaflet/dist/leaflet.css";
-import chile from "./../assets/comunas.json";
+import "./Map.css";
+import pais from "../assets/chile.json";
+import regiones from "../assets/regiones.json";
+import comunas from "../assets/comunas.json";
 
-function Map() {
-  console.log(chile);
+const defaultStyle = {
+  fillColor: "#ff7800", // Color original de relleno
+  weight: 2,
+  opacity: 1,
+  color: "white",
+  dashArray: "3",
+  fillOpacity: 0.7,
+};
+const clickStyle = {
+  fillColor: "red",
+  weight: 2,
+  opacity: 1,
+  color: "white",
+  dashArray: "3",
+  fillOpacity: 0.7,
+};
 
-  const highlightFeature = (e: any) => {
-    const layer = e.target;
-    layer.setStyle({
-      fillColor: "blue",
-      weight: 2,
-      opacity: 1,
-      color: "white",
-      dashArray: "3",
-      fillOpacity: 0.7,
+export function Map() {
+  const paisLayer = L.geoJSON(pais, { style: defaultStyle });
+  const regionLayer = L.geoJSON(regiones, { style: defaultStyle });
+  const comunaLayer = L.geoJSON(comunas, { style: defaultStyle });
+
+  const [currentZoom, setCurrentZoom] = useState(4);
+  const [selectPais, setSelectPais] = useState(null);
+  const [selectRegion, setSelectRegion] = useState(regionLayer); // Cambio de nombre para mayor claridad
+  const [selectComuna, setSelectComuna] = useState(comunaLayer);
+
+  const zoomToLayerMap = {
+    4: [
+      paisLayer,
+      L.latLngBounds(L.latLng(-10, -110.0), L.latLng(-60.0, -40.0)),
+    ],
+    6: [
+      regionLayer,
+      L.latLngBounds(L.latLng(-17, -65.0), L.latLng(-58.0, -77.0)),
+    ],
+    8: [
+      comunaLayer,
+      L.latLngBounds(L.latLng(-16, -65.0), L.latLng(-58.0, -78.0)),
+    ],
+  };
+
+  useEffect(() => {
+    const map = L.map("map", {
+      minZoom: 4,
+      maxZoom: 8,
+      zoomSnap: 2,
+      zoomDelta: 2,
+      maxBounds: L.latLngBounds(L.latLng(-10, -110.0), L.latLng(-60.0, -40.0)),
+    }).setView([-35.675147, -71.542969], 4);
+
+    L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      maxZoom: 20,
+      minZoom: 4,
+    }).addTo(map);
+
+    let currentLayer = paisLayer.addTo(map);
+
+    map.on("zoomend", (e) => {
+      const zoom = e.target.getZoom();
+      setCurrentZoom(zoom);
+      const [newLayer, maxBounds] = zoomToLayerMap[zoom];
+      if (newLayer && currentLayer !== newLayer) {
+        map.removeLayer(currentLayer);
+        currentLayer = newLayer.addTo(map);
+        map.setMaxBounds(maxBounds);
+      }
     });
-  };
 
-  const resetHighlight = (e: any) => {
-    const layer = e.target;
-    layer.setStyle({
-      fillColor: "green",
-      weight: 1,
-      opacity: 1,
-      color: "black",
-      fillOpacity: 0.5,
-    });
-  };
+    var info = L.control();
 
-  const showPopup = (feature: any, layer: any) => {
-    layer.bindPopup(`<div>${feature.properties.Comuna}</div>`);
-  };
+    info.onAdd = function (map) {
+      this._div = L.DomUtil.create("div", "info"); // create a div with a class "info"
+      this.update();
+      return this._div;
+    };
 
-  const onEachFeature = (feature: any, layer: any) => {
-    layer.on({
-      mouseover: highlightFeature,
-      mouseout: resetHighlight,
-      click: (e: any) => {
-        showPopup(feature, layer);
-        console.log(e);
-      },
-    });
-  };
+    info.update = function (props) {
+      this._div.innerHTML =
+        "<h4>Información</h4>" +
+        (props ? "<b>" + props.name + "</b><br />" : "Click sobre el mapa");
+    };
 
-  return (
-    <MapContainer center={[-39.808333, -73.241667]} zoom={9} scrollWheelZoom={false}>
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <GeoJSON
-        data={chile as any} // Añade "as any" para evitar errores de tipo
-        onEachFeature={onEachFeature}
-      />
-    </MapContainer>
-  );
+    info.addTo(map);
+
+    const handleLayerClick = (e, property) => {
+      const name = e.layer.feature.properties[property];
+      console.log(name);
+      info.update({ name: name });
+      if (property === "COUNTRY") {
+        if (selectPais) {
+          selectPais.setStyle(defaultStyle);
+        }
+        e.layer.setStyle(clickStyle);
+        setSelectPais(e.layer);
+      } else if (property === "Region") {
+        if (selectRegion) {
+          selectRegion.setStyle(defaultStyle);
+        }
+        e.layer.setStyle(clickStyle);
+        setSelectRegion(e.layer);
+      } else if (property === "Comuna") {
+        if (selectRegion) {
+          selectComuna.setStyle(defaultStyle);
+        }
+        e.layer.setStyle(clickStyle);
+        setSelectComuna(e.layer);
+      }
+    };
+
+    paisLayer.on("click", (e) => handleLayerClick(e, "COUNTRY"));
+    regionLayer.on("click", (e) => handleLayerClick(e, "Region"));
+    comunaLayer.on("click", (e) => handleLayerClick(e, "Comuna"));
+
+    return () => {
+      map.remove();
+    };
+  }, []);
+
+  return <div id="map" className="Map-container"></div>;
 }
 
 export default Map;
