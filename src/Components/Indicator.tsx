f326d712f40a13d42164f00ec82ad156f5d1c13a
FelipePrieto22 <felipeprieto2321@gmail.com>
2023-09-14 14:53:40 -0300

    componentes y caracteristicas generales listas, no avanzo mas hasta la vuelta del receso,en receso no pesco weones

--- src/Components/Indicator.tsx
+++ src/Components/Indicator.tsx
@@ -1,6 +1,6 @@
-import { COLOR_WELFARE } from "../constants/styles.constants";
+import { COLOR_WELFARE, clickStyle } from "../constants/styles.constants";
 import { useState } from "react";
-import dot from "../assets/dot.svg";
+import Arrow from "../assets/Arrow";
 
 const Indicator = ({ progress, imageUrl, name, onClick, isSelected }) => {
   const [clickButton, setClickButton] = useState(false);
@@ -25,49 +25,64 @@ const Indicator = ({ progress, imageUrl, name, onClick, isSelected }) => {
   };
 
   return (
-    <div
-      className={`h-screen-[50%] flex m-[1vh] p-[1vh] items-center rounded-lg ${
-        isSelected ? "bg-green-400" : ""
-      }`}
-    >
-      <div>
-        <button
-          className="rounded-lg overflow-hidden hover:bg-blue-500"
-          onClick={handleButtonClick}
-        >
-          <img
-            className={`h-[3vw] ${isSelected ? "bg-white" : ""}`}
-            src={imageUrl}
-            alt={name}
-          />
-        </button>
-      </div>
+    <>
+      <div
+        className={`h-screen-[50%] flex m-[1vh] p-[1vh] items-center rounded-lg ${
+          isSelected ? "bg-green-400" : ""
+        }`}
+      >
+        <div>
+          <button
+            className="rounded-lg overflow-hidden hover:bg-blue-500"
+            onClick={handleButtonClick}
+          >
+            <img
+              className={`h-[3vw] ${isSelected ? "bg-white" : ""}`}
+              src={imageUrl}
+              alt={name}
+            />
+          </button>
+        </div>
 
-      <div className="grid items-center w-full ml-2 bg-white rounded-full h-1/2 font-bold">
-        <div
-          className="rounded-full text-[1vw]"
-          style={{
-            width: `${progress}%`,
-            backgroundColor: getColor(),
-          }}
-        >
-          {Math.round(progress)}%
+        <div className="grid items-center w-full ml-2 bg-white rounded-full h-1/2 font-bold">
+          <div
+            className="rounded-full text-[1vw]"
+            style={{
+              width: `${progress}%`,
+              backgroundColor: getColor(),
+            }}
+          >
+            {Math.round(progress)}%
+          </div>
+        </div>
+        <div className="flex items-center ml-1 rounded-lg">
+          <button onClick={handleClick}>
+            <div
+              className={`transform h-8 ml-1 ${
+                !clickButton ? "rotate-90" : ""
+              }`}
+            >
+              <Arrow color={`${clickButton ? "red" : "black"}`} />
+            </div>
+          </button>
         </div>
       </div>
-      <div className="flex items-center ml-1 rounded-lg">
-        <button
-          className={`rounded-lg hover:bg-blue-500 ${
-            clickButton ? "bg-white" : ""
-          }`}
-        >
-          <img
-            src={dot}
-            className="transform rotate-90"
-            onClick={handleClick}
-          />
-        </button>
+      <div className={`grid ${!clickButton ? "hidden" : ""}`}>
+        <table>
+          <thead>
+            <tr>
+              <th>Variables:</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>Indicator 1</tr>
+            <tr>Indicator 2</tr>
+            <tr>Indicator 3</tr>
+            <tr>Indicator n</tr>
+          </tbody>
+        </table>
       </div>
-    </div>
+    </>
   );
 };
 
